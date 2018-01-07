bb2719b001a0a42d37f394ced7929457f6770d5a
anishpanthi <anish.panthi@gmail.com>
2018-01-07 17:47:22 -0600

    Initial Commit

---
+++ user-mgmt-weblogic/src/main/java/com/app/usermanagement/api/UserMgmtWeblogicApplication.java
@@ -0,0 +1,12 @@
+package com.app.usermanagement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtWeblogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtWeblogicApplication.class, args);
+	}
+}
