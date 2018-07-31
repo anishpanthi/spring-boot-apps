15f1649595ec6328ae8fbaca6d254042b9f608d1
Anish Panthi <anish@Anishs-MacBook-Pro.local>
2018-07-30 20:25:18 -0500

    Updated Initializer class to work with WebLogic.

--- boot-swagger2/src/main/java/com/app/boot/swagger2/api/BootSwagger2Application.java
+++ boot-swagger2/src/main/java/com/app/boot/swagger2/api/BootSwagger2Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import sun.tools.jar.CommandLine;
 
 import java.util.ArrayList;
 import java.util.List;
--- boot-swagger2/src/main/java/com/app/boot/swagger2/api/ServletInitializer.java
+++ boot-swagger2/src/main/java/com/app/boot/swagger2/api/ServletInitializer.java
@@ -2,8 +2,9 @@ package com.app.boot.swagger2.api;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
