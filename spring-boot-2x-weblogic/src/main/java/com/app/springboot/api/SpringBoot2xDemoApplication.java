11ceebecdc3c5143e713dbeb86e99788fcd1b057
Anish Panthi <anish.panthi@hms.com>
2018-12-15 14:24:55 -0600

    Initial Repo for WebLogic 12.1.2.1

---
+++ spring-boot-2x-weblogic/src/main/java/com/app/springboot/api/SpringBoot2xDemoApplication.java
@@ -0,0 +1,14 @@
+package com.app.springboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2xDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2xDemoApplication.class, args);
+    }
+
+}
+
