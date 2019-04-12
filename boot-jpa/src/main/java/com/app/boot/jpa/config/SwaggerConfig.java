768b8e3d16a680706f79fa009af48bec36770421
Anish Panthi <anish@Anishs-MacBook-Pro.local>
2019-04-11 23:51:55 -0500

    Added Authorization header field from Swagger Configuration

--- boot-jpa/src/main/java/com/app/boot/jpa/config/SwaggerConfig.java
+++ boot-jpa/src/main/java/com/app/boot/jpa/config/SwaggerConfig.java
@@ -7,12 +7,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 /**
  * @author Anish
@@ -41,7 +45,15 @@ public class SwaggerConfig {
     @Bean
     public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage(basePackage))
-                .paths(PathSelectors.ant("/**")).build().apiInfo(metaData());
+                .paths(PathSelectors.ant("/**"))
+                .build().apiInfo(metaData())
+                .globalOperationParameters(Arrays.asList(new ParameterBuilder()
+                        .name("Authorization")
+                        .description("JWT Token with the format \"Bearer <actual_jwt_token>\"")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(true)
+                        .build()));
     }
 
     private ApiInfo metaData() {
